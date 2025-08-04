9e6a82c6f30939a6049f2860fcba7d95d79516d6
Ahmad Alkhooly <ahmadalkhooly@Ahmads-MacBook-Pro.local>
2025-08-05 00:13:39 +0300

    trying to impement searching

---
+++ dist/18-forkify.62406edb.js
@@ -0,0 +1,33 @@
+const recipeContainer = document.querySelector('.recipe');
+const searchedValue = document.querySelector('.search');
+const timeout = function(s) {
+    return new Promise(function(_, reject) {
+        setTimeout(function() {
+            reject(new Error(`Request took too long! Timeout after ${s} second`));
+        }, s * 1000);
+    });
+};
+// https://forkify-api.herokuapp.com/v2
+///////////////////////////////////////
+const desh = new Promise(function(resolve, reject) {
+    searchedValue.addEventListener('keydown', function(e) {
+        if (e.key === 'Enter') {
+            e.preventDefault();
+            resolve(e.target.closest('input').value);
+            e.target.closest('input').value = '';
+        }
+    });
+});
+const Recipe = async function(dish) {
+    try {
+        // const searchLabelData = ;
+        const api = await fetch(`https://forkify-api.jonas.io/api/v2/recipes?search=${dish}`);
+        const data = await api.json();
+        console.log(data);
+    } catch (err) {
+        console.log(`Something wrong ${err.message}`);
+    }
+};
+desh.then((e)=>Recipe(e));
+
+//# sourceMappingURL=18-forkify.62406edb.js.map
--- src/js/controller.js
+++ src/js/controller.js
@@ -1,5 +1,5 @@
 const recipeContainer = document.querySelector('.recipe');
-
+const searchedValue = document.querySelector('.search')
 const timeout = function (s) {
   return new Promise(function (_, reject) {
     setTimeout(function () {
@@ -7,7 +7,29 @@ const timeout = function (s) {
     }, s * 1000);
   });
 };
-
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+
+const desh = new Promise(function(resolve,reject){
+  searchedValue.addEventListener('keydown',function(e){
+    if(e.key === 'Enter'){
+      e.preventDefault();
+      resolve(e.target.closest('input').value);
+      e.target.closest('input').value = '';
+    }
+  })
+  
+})
+const Recipe = async function(dish){
+  try{
+    // const searchLabelData = ;
+    const api = await fetch(`https://forkify-api.jonas.io/api/v2/recipes?search=${dish}`);
+    const data = await api.json();
+    console.log(data);
+  } catch(err){
+      console.log(`Something wrong ${err.message}`);
+  }  
+}
+
+desh.then(e => Recipe(e))
