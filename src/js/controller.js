5f4a6c7cb76cb5fed24dd5e6a72dcc0895bc3c8a
Ahmad Alkhooly <ahmadalkhooly@Ahmads-MacBook-Pro.local>
2025-08-06 23:00:18 +0300

    new Branch

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,7 +1,7 @@
 const recipeContainer = document.querySelector('.recipe');
-const searchedValue = document.querySelector('.search');
-const searchedResults = document.querySelector('.search-results');
-const everySingleRecipe = document.querySelector('.preview');
+// const searchedValue = document.querySelector('.search');
+// const searchedResults = document.querySelector('.search-results');
+// const everySingleRecipe = document.querySelector('.preview');
 const timeout = function (s) {
   return new Promise(function (_, reject) {
     setTimeout(function () {
@@ -14,49 +14,27 @@ const timeout = function (s) {
 ///////////////////////////////////////
 
 
-const renderRecipes = function(data){
-  const markup = data.data.recipes.map(element => {
-    return `
-       <li class="preview">
-            <a class="preview__link preview__link--active" href="#23456">
-              <figure class="preview__fig">
-                <img src="${element.image_url}" alt="Test" />
-              </figure>
-              <div class="preview__data">
-                <h4 class="preview__title">${element.title}</h4>
-                <p class="preview__publisher">${element.publisher}</p>
-                <div class="preview__user-generated">
-                  <svg>
-                    <use href="src/img/icons.svg#icon-user"></use>
-                  </svg>
-                </div>
-              </div>
-            </a>
-          </li>
-    `;
-  }).join('');
-  searchedResults.innerHTML = markup;
-}
-const showError = (msg) => searchedResults.innerHTML = `<h1 style ="text-align:center">${msg}</h1>`;
-const Recipe = async function(dish){
+const showRecipe = async function(){
   try{
-    // const searchLabelData = ;
-    const api = await fetch(`https://forkify-api.jonas.io/api/v2/recipes?search=${dish}`);
-    const data = await api.json();
-    renderRecipes(data)
-    if(data.data.recipes.length === 0) showError('No recipes found 😊')
-    console.log(data);
+    const res = await fetch('https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886');
+    const data = await res.json();
+
+    if(!res.ok) throw new Error(`${data.message} (${res.status})`)
+    
+    let {recipe} = data.data;
+    recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      cookingTimes: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+      servings: recipe.servings,
+    } 
+    console.log(recipe);
   } catch(err){
-      console.log(`Something wrong ${err.message}`);
-  }  
-}
-
-searchedValue.addEventListener('keydown',function(e){
-  if(e.key === 'Enter'){
-    e.preventDefault();
-    Recipe(e.target.value);
-    e.target.value = '';
-    searchedResults.innerHTML = '';
+    alert(err)
   }
-})
-
+}
+showRecipe();
